c67eb62d54a6a9463237ec39b81147a78356e6be
Hiroshi Mimaki <hiroshi_mimaki@scsk-kyushu.co.jp>
2017-05-12 11:47:05 +0900

    Modify main.cpp and app.rb

--- app.rb
+++ app.rb
@@ -1,2 +1,1 @@
-puts 'Hello, mbed-mruby!'
-[1,4,7,2,5,8,3,6,9].sort.reverse
+puts "mruby-#{MRUBY_VERSION} on mbed"
--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@
 #include "mruby/_string.h"
 #include "mbedapi.h"
 
-// for test
 DigitalOut ledr(LED1);
 DigitalOut ledg(LED2);
 DigitalOut ledb(LED3);
@@ -15,31 +14,31 @@ int main(void)
 {
   extern const uint8_t appbin[];
   mrb_value v, s;
-  mrb_state *mrb = mrb_open();
-
-  mbedPrintf("Hello, mbed!\n");
-
-  ledr = 1;
-  v = mrb_load_irep(mrb, appbin);
-  s = mrb_obj_as_string(mrb, v);
-  mbedPrintf(mrb_str_to_cstr(mrb, s));
-  mbedPrintf("\n");
-  ledg = 1;
-
-  wait(1);
-  ledr = 0;
-  ledg = 0;
-
-  mirb(mrb);
-
-  mbedPrintf("mirb done.\n");
-
+  mrb_state *mrb;
+  
   while (1) {
-      ledb = !ledb;
-      wait(0.1);
-  }
+    ledr = 1;
+    mrb = mrb_open();
 
-  mrb_close(mrb);
+    wait(0.1);
+    ledg = 1;
+
+    /* launch mruby application */
+    v = mrb_load_irep(mrb, appbin);
+    s = mrb_obj_as_string(mrb, v);
+    mbedPrintf(mrb_str_to_cstr(mrb, s));
+    mbedPrintf("\n");
+
+    wait(0.1);
+    ledb = 1;
+
+    wait(0.3);
+    ledr = 0; ledg = 0; ledb = 0;
+    /* lauch mirb */
+    mirb(mrb);
+
+    mrb_close(mrb);
+  }
 
   return 0;
 }
