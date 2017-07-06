97aa9d8f8715887e7da0dc9f1a161f9b56fee9ea
Hiroshi Mimaki <hiroshi_mimaki@scsk-kyushu.co.jp>
2017-07-06 10:26:18 +0900

    Add patch/unpatch for mruby

--- Makefile
+++ Makefile
@@ -1,14 +1,19 @@
 MRUBY = ./mruby
 
 all:
+	ruby patch-mruby.rb
 	MRUBY_CONFIG=../mbed_build_config.rb make -C $(MRUBY)
 	$(MRUBY)/bin/mrbc -Bappbin -o app.c app.rb
 	mbed compile
+	ruby unpatch-mruby.rb
 
 test:
+	ruby patch-mruby.rb
 	MRUBY_CONFIG=../mbed_build_config.rb make -C $(MRUBY) test
+	ruby unpatch-mruby.rb
 
 clean:
 	make -C $(MRUBY) clean
 	rm -rf mruby/build
 	rm -f app.c
+	ruby unpatch-mruby.rb
---
+++ unpatch-mruby.rb
@@ -0,0 +1,24 @@
+#! /usr/bin/env ruby
+require 'fileutils'
+
+# remame 'mruby/string.h' to 'mruby/_string.h'
+inc = File.join('mruby', 'include', 'mruby')
+string_h = File.join(inc, 'string.h')
+_string_h = File.join(inc, '_string.h')
+if File.exist? _string_h
+  FileUtils.mv _string_h, string_h
+end
+
+bak = case RUBY_PLATFORM.downcase
+when /darwin/ # macOS
+  "''"
+else  # Linux and other
+  ''
+end
+
+# modify #inlucde path
+`find mruby -type f | grep '\.[ch]$'`.each_line {|f|
+  if f =~ /\.[ch]$/
+    `sed -i #{bak} 's!mruby/_string.h!mruby/string.h!g' #{f}`
+  end
+}
