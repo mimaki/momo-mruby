38feaad4700a13d28722add6374275f755be6918
Hiroshi Mimaki <hiroshi_mimaki@scsk-kyushu.co.jp>
2017-07-12 13:36:59 +0900

    Add SDFileSystem

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include "mbed.h"
+#include "SDFileSystem.h"
+
 #include "mruby.h"
 #include "mruby/irep.h"
+#include "mruby/dump.h"
 #include "mruby/_string.h"
 #include "mbedapi.h"
+
+/* mount microSD */
+SDFileSystem sd(P8_5, P8_6, P8_3, P8_4, "sd");
 
 DigitalOut ledr(LED1);
 DigitalOut ledg(LED2);
@@ -15,7 +21,8 @@ int main(void)
   extern const uint8_t appbin[];
   mrb_value v, s;
   mrb_state *mrb;
-  
+  FILE *fp;
+
   while (1) {
     ledr = 1;
     mrb = mrb_open();
@@ -23,8 +30,15 @@ int main(void)
     wait(0.1);
     ledg = 1;
 
-    /* launch mruby application */
-    v = mrb_load_irep(mrb, appbin);
+    if ((fp = fopen("/sd/app.mrb", "rb")) != NULL) {
+      /* launch mruby application (MRB file) */
+      v = mrb_load_irep_file(mrb, fp);
+      fclose(fp);
+    }
+    else {
+      /* launch mruby application (on memory) */
+      v = mrb_load_irep(mrb, appbin);
+    }
     s = mrb_obj_as_string(mrb, v);
     mbedPrintf(mrb_str_to_cstr(mrb, s));
     mbedPrintf("\n");
@@ -34,6 +48,7 @@ int main(void)
 
     wait(0.3);
     ledr = 0; ledg = 0; ledb = 0;
+
     /* lauch mirb */
     mirb(mrb);
 
